ef1301a89a1c0027eb048454c6fbd41e4e6ddb15
Shay Perchik <perchik.shay@gmail.com>
2015-04-17 18:46:44 +0300

    Add ip scan using ping, port scanniing using socket

--- Excr2.py
+++ Excr2.py
@@ -1,39 +1,78 @@
 import os
 import argparse
 import socket
+import struct
 
-def IsIpValid(IP):
+def GetBinaryIP(IP):
 	try:
-   			socket.inet_aton(IP)
-   			return True
+		return struct.unpack("!I", socket.inet_aton(IP))[0]
    	# legal
 	except socket.error:
-			return False
+			return 0
    	# Not legal
 
 def PingServer(IP):
 	response = os.system("ping -c 1 " + IP)
 	#and then check the response...
 	if response == 0:
-		print IP, 'is up!'
+		print '\n******* ' + IP, 'is up! *******'
 		return True
 	else:
-		print IP, 'is down!'
+		print '\n' + IP, 'is down!'
 		return False
 
+def GetStringIP(IP):
+	try:
+		packed_value = struct.pack('!I', IP)
+		addr = socket.inet_ntoa(packed_value)
+		return addr
+   	#legal
+	except socket.error:
+		return "NULL"
+	#Not legal
+
+def CheckOpenTcpPort(IP, PORT):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		s.connect((IP, PORT))	
+		s.close()
+		return True
+	except Exception, e:
+		print('Port Closed: ', PORT)
+		s.close()
+    	return False
 def main():
+	HTTP_PORT = 80
+	SSH_PORT = 22
+	FTP_PORT = 21
 	parser = argparse.ArgumentParser(description='Get IP and subnet mask for scan.')
 	parser.add_argument('-IP','--IP',action='store', help='Set your ip address. inet(3) format', default="192.168.1.1")
-	parser.add_argument('-subnet','--subnet',action='store', help='Set the subnet mask to check. inet(3) format',default="255.255.255.0")
+	parser.add_argument('-subnet','--subnet',action='store', help='We support only 255.255.255.0 in this version',default="255.255.255.0")
 
 	args = parser.parse_args()
-	if IsIpValid(args.IP) != True:
+	BinaryIP = GetBinaryIP(args.IP)
+	BinaryMask = GetBinaryIP(args.subnet)
+	if BinaryIP == 0:
 		print("Ip is wrong, try again")
-	if IsIpValid(args.subnet) != True:
+		return
+	if (BinaryMask == 0 or args.subnet != '255.255.255.0'):
 		print("Subnet is wrong, try again")
+		return
+	NetWorkMaskToScan = (BinaryIP & BinaryMask)
+	NetworkIP = GetStringIP(NetWorkMaskToScan)
+	print("Subnet To Scan: " + NetworkIP)
 
-	PingServer(args.IP)
-
+	for i in range(255):
+		IpToScan = NetWorkMaskToScan | i
+		StringIpToScan = GetStringIP(IpToScan)
+		print("Scanning ip: " + StringIpToScan)
+		if (PingServer(StringIpToScan) == True):
+			if(CheckOpenTcpPort(StringIpToScan,HTTP_PORT) == True):
+				print('ip: ' + StringIpToScan + ' HTTP Port Is open:)')
+			if(CheckOpenTcpPort(StringIpToScan,FTP_PORT) == True):
+				print('ip: ' + StringIpToScan + ' FTP Port Is open:)')
+			if(CheckOpenTcpPort(StringIpToScan,SSH_PORT) == True):
+				print('ip: ' + StringIpToScan + ' SSH Port Is open:)')
 
 if __name__ == "__main__":
    main()
